3d98d8dadfded4f92f2d427c60c85c0aa3b9980a
David Allison <62114487+david-allison@users.noreply.github.com>
2023-02-03 02:53:13 +0000

    fix: too many audio players
    This caused distorted audio

--- ManxAudioSearch/ManxAudioSearch/ClientApp/src/components/AudioPlayer.js
+++ ManxAudioSearch/ManxAudioSearch/ClientApp/src/components/AudioPlayer.js
@@ -1,16 +1,38 @@
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export const AudioPlayer = (props) => {
-    
+    // loading too many audio files causes issues with Chrome - only load if we play
+    // This causes complexity here
+    const [clicked, setClicked] = useState(false)
+    const [autoPlayed, setAutoPlayed] = useState(false)
     const ref = useRef()
+    
     const onClick = () => {
-        console.log("onClick")
+        if (!clicked) {
+            setClicked(true)
+            console.log(!!ref.current)
+            return
+        }
         ref.current.play()
     }
+    
+    // another hack: once clicked is true, we can play
+    useEffect(() => {
+        if (!clicked || autoPlayed) {
+            return
+        }
+        setAutoPlayed(true) // a refresh shouldn't play 1000 sounds 
+        if (ref.current) {
+            ref.current.play()
+        } else {
+            console.warn("problem?")
+        }
+    }, [clicked])
+    
     return (<>
-        <audio preload={"metadata"} ref={ref}>
+        {clicked && <audio preload={"metadata"} ref={ref}>
             <source src={`/audio/${props.fileName}`}/>
-        </audio>
+        </audio>}
         <img style={{height: "1em"}} src={require("../assets/images/volume-high.png")} onClick={onClick}/>
     </>)
 }
